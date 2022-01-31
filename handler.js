e8e883536c9ae3d4e7c734d87d9ff20ee30acb66
Marat Bakeev <marat.bakeev@whitehatgaming.com>
2022-01-31 22:00:57 +1300

    more edits

--- handler.js
+++ handler.js
@@ -4,11 +4,18 @@ const fn = require('modules/checktags.js')
 
 module.exports.hello = async (event) => {
   console.log(event);
+  let paragraph = '';
+  if (event.body) {
+    let body = JSON.parse(event.body)
+    if (body.paragraph)
+      paragraph = body.paragraph;
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: fn.checkTags(event),
+        message: fn.checkTags(paragraph),
         input: event,
       },
       null,
