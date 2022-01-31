15acc8e0d45f0eabe59608b12e2de20533ee5219
Marat Bakeev <marat.bakeev@whitehatgaming.com>
2022-02-01 11:26:28 +1300

    new line

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,4 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 echo "Deploying to $env"
 echo "_______________________________"
 serverless deploy --stage $env --region ap-southeast-2 --package $CODEBUILD_SRC_DIR/artifacts/$env
+
